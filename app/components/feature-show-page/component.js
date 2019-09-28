8b170b28ef4a510fdf431bd767330b7ebe84686e
Joan Racenet <joan.racenet@hiptest.net>
2019-09-28 16:08:05 +0200

    Can update a gherkin feature

--- app/components/feature-show-page/component.js
+++ app/components/feature-show-page/component.js
@@ -1,4 +1,15 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
+import { isPresent } from '@ember/utils';
 
 export default Component.extend({
+  isGherkinChanged: computed('feature.hasDirtyAttributes', 'feature.content', function () {
+    return isPresent(this.feature.changedAttributes().content)
+  }),
+
+  actions: {
+    save() {
+      this.feature.save()
+    }
+  }
 });
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -2,9 +2,13 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function(defaults) {
+module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
+    ace: {
+      themes: ['ambiance', 'chaos'],
+      modes: ['gherkin'],
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
